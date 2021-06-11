b6ca419877bdca1cfc718c201d911c8d88cc32eb
yusalie <yusuf.salie96@gmail.com>
2021-06-11 16:24:14 +0200

    continue this project

---
+++ variables.java
@@ -0,0 +1,9 @@
+//name variables using lowerCamelCase
+// eg numberOfVariables = 1;
+//left off trying to get user input
+public class variables {
+    public static void main(String[] args) {
+        int number = 20;
+        System.out.print(number);
+    }
+}
