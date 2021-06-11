50e1b99ab59f8a21a79edb5c667e6e6568d1518b
yusalie <yusuf.salie96@gmail.com>
2021-06-11 12:34:34 +0200

    first commit

---
+++ HelloJava.java
@@ -0,0 +1,8 @@
+// semicolon means end of code
+// use semicolon at the end of each block of code
+
+public class HelloJava {
+    public static void main(String[] args) {
+      System.out.println("Hello Java");
+    }
+}
