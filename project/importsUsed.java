c5f48b858cf73869cf07bf6fc843e0ea9ec2d99e
yusalie <yusuf.salie96@gmail.com>
2021-06-18 13:56:20 +0200

    update

--- project/importsUsed.java
+++ project/importsUsed.java
@@ -2,6 +2,9 @@ package project;
 import java.awt.FlowLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class importsUsed extends JFrame{
     private JLabel item1;
@@ -9,7 +12,14 @@ public class importsUsed extends JFrame{
         super("The title");
         setLayout(new FlowLayout());
         item1 = new JLabel("This is a label");
-        item1.setToolTipText("This shows on hover");
+        JButton btn = new JButton("click me");
+        btn.setBounds(50, 50, 50, 50);
         add(item1);
+        add(btn);
+        btn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                item1.setText("You clicked the button");
+            }
+        });
     }
 }
